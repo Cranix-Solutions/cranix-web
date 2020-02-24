128fe253335cacdfc0f1964479b363b2351090e0
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-02-24 12:59:49 +0100

    Implementing default values. Implementing slections. Implementing error handling.

--- src/app/shared/objects-edit/objects-edit.component.ts
+++ src/app/shared/objects-edit/objects-edit.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavParams } from '@ionic/angular';
+import { ModalController, NavParams, ToastController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //own 
-import {GenericObjectService} from '../../services/generic-object.service';
+import { GenericObjectService } from '../../services/generic-object.service';
 import { User, Group, Room, Device, HWConfig, UserC } from '../models/data-model';
 import { Institute, Customer } from '../models/cephalix-data-model';
+import { async } from 'rxjs/internal/scheduler/async';
+import { ServerResponse } from '../models/server-models';
 @Component({
   selector: 'cranix-objects-edit',
   templateUrl: './objects-edit.component.html',
@@ -14,19 +16,35 @@ import { Institute, Customer } from '../models/cephalix-data-model';
 export class ObjectsEditComponent implements OnInit {
 
   result: any = {};
+  /**
+   * This hash will contains the default values for some enumerated values like role, network ...
+   */
+  defaults: any = {
+    'role': ['teachers', 'students', 'sysadmins']
+  }
+    ;
+  /**
+   * This hash defines if a key  has defaults
+   */
+  hasDefaults: any = {};
   editForm: FormGroup;
   objectType: string = "";
   object: any = null;
   objectKeys: string[] = [];
   objectActionTitle: string = "";
   objectAction = "";
+  /**
+   * Attributes which can not be modified.
+   */
   readOnlyAttributes: string[] = [
     'fsQuotaUsed',
     'msQuotaUsed',
     'recDate',
     'role',
   ]
-
+  /**
+   * Attributes which we get but need not be shown
+   */
   hiddenAttributes: string[] = [
     'id',
     'ownerId',
@@ -39,7 +57,8 @@ export class ObjectsEditComponent implements OnInit {
     private navParams: NavParams,
     private modalController: ModalController,
     public translateService: TranslateService,
-    public objectService: GenericObjectService
+    public objectService: GenericObjectService,
+    public toastController: ToastController
   ) {
     this.objectType = this.navParams.get('objectType');
     this.object = this.navParams.get('object');
@@ -63,7 +82,7 @@ export class ObjectsEditComponent implements OnInit {
    */
   typeOf(key) {
     let obj = this.object[key];
-    if (key == 'birthDay'|| key == 'validity' || key=='recDate' || key == 'validFrom' || key == 'validUntil' ) {
+    if (key == 'birthDay' || key == 'validity' || key == 'recDate' || key == 'validFrom' || key == 'validUntil') {
       let d = new Date()
       return "date";
     }
@@ -87,26 +106,53 @@ export class ObjectsEditComponent implements OnInit {
   }
 
   onSubmit(object) {
-    this.objectService.applyAction(object,this.objectType,this.objectAction).subscribe(
+    let serverResponse: ServerResponse;
+    this.objectService.applyAction(object, this.objectType, this.objectAction).subscribe(
       (val) => {
-          console.log(val);
+        serverResponse = val;
+        console.log(val);
       },
-      (error) => {
-
+      async (error) => {
+        const toast = this.toastController.create({
+          position: "middle",
+          message: "An Server Error is accoured",
+          cssClass: "bar-assertive",
+          duration: 3000
+        });
+        (await toast).present();
       },
-      () => {
-        this.modalController.dismiss();
+      async () => {
+        if (serverResponse.code == "OK") {
+          const toast = this.toastController.create({
+            position: "middle",
+            header: "Success:",
+            message: serverResponse.value,
+            color: "success",
+            duration: 5000
+          });
+          (await toast).present();
+          this.modalController.dismiss();
+        } else {
+          const toast = this.toastController.create({
+            position: "middle",
+            header: "An Error was accoured:",
+            message: serverResponse.value,
+            color: "danger",
+            duration: 3000
+          });
+          (await toast).present();
+        }
       }
-      )
-    console.log(object);
+    )
+
   }
 
-  convertObject(){
+  convertObject() {
     //TODO introduce checks
-    let output: any ={};
-    for(let key in this.object){
-      if(key == 'birthDay' || key == 'validity' || key=='recDate' || key == 'validFrom' || key == 'validUntil' ){
-        let date =  new Date(this.object[key]);
+    let output: any = {};
+    for (let key in this.object) {
+      if (key == 'birthDay' || key == 'validity' || key == 'recDate' || key == 'validFrom' || key == 'validUntil') {
+        let date = new Date(this.object[key]);
         output[key] = date.toJSON();
       } else {
         output[key] = this.object[key];
