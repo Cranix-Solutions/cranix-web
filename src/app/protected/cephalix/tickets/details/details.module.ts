1d400ad06f2a7501748749d2c6a55aa6fcbe847f
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-03-05 08:26:47 +0100

    Add missed module

---
+++ src/app/protected/cephalix/tickets/details/details.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { DetailsPageRoutingModule } from './details-routing.module';
+import { DetailsPage } from './details.page';
+import { TranslateService } from '@ngx-translate/core';
+
+import {CranixSharedModule} from '../../../../shared/cranix-shared.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DetailsPageRoutingModule,
+    CranixSharedModule
+  ],
+  declarations: [DetailsPage],
+  providers: [TranslateService]
+})
+export class DetailsPageModule {}
