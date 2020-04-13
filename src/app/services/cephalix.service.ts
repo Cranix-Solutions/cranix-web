e1480cda514995ae897038cbd34068b9511f21f0
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-04-13 22:30:21 +0200

    Use this.headers

--- src/app/services/cephalix.service.ts
+++ src/app/services/cephalix.service.ts
@@ -92,144 +92,83 @@ export class CephalixService {
 	getArticklesOfTicket(id: number): Observable<Article[]> {
 		this.url = this.hostname + '/tickets/' + id + '/articles';
 		console.log(this.url);
-		const headers = new HttpHeaders({
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.get<Article[]>(this.url, { headers: headers });
+		return this.http.get<Article[]>(this.url, { headers: this.headers });
 	};
 
 
 	getInstituteTypes(): Observable<string[]> {
 		this.url = this.hostname + `/system/enumerates/institutetype`;
 		console.log(this.url);
-		const headers = new HttpHeaders({
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.get<string[]>(this.url, { headers: headers });
+		return this.http.get<string[]>(this.url, { headers: this.headers });
 	}
 
 	getCategorieTypes() {
-		//	this.hostname = this.utils.hostName();
-		//      this.token = this.token;
 		this.url = this.hostname + `/institutes/objects`;
 		console.log(this.url);
-		const headers = new HttpHeaders({
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.get<string[]>(this.url, { headers: headers });
+		return this.http.get<string[]>(this.url, { headers: this.headers });
 	}
 
 	getInstituteById(id: number){
 		this.url = this.hostname + `/institutes/${id}`;
 		console.log(this.url);
-		const headers = new HttpHeaders({
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.get<Institute>(this.url, { headers: headers });
+		return this.http.get<Institute>(this.url, { headers: this.headers });
 	}
 
 
 	getAyTemplates(){
 		this.url = this.hostname + `/institutes/ayTemplates`;
 		console.log(this.url);
-		const headers = new HttpHeaders({
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.get<string[]>(this.url, { headers: headers });
+		return this.http.get<string[]>(this.url, { headers: this.headers });
 	}
 	getNoticesOfInst(id: number){
-			//	this.hostname = this.utils.hostName();
-		//      this.token = this.token;
 		this.url = this.hostname + `/institutes/${id}/notices`;
 		console.log(this.url);
-		const headers = new HttpHeaders({
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.get<Notice[]>(this.url, { headers: headers });
+		return this.http.get<Notice[]>(this.url, { headers: this.headers });
 	}
 	getOssCaerOfInst(id: number){
-			//	this.hostname = this.utils.hostName();
-		//      this.token = this.token;
 		this.url = this.hostname + `/customers/institutes/${id}/osscare`;
 		console.log(this.url);
 		const headers = new HttpHeaders({
 			'Accept': "application/json",
 			'Authorization': "Bearer " + this.token
 		});
-		return this.http.get<OssCare[]>(this.url, { headers: headers });
+		return this.http.get<OssCare[]>(this.url, { headers: this.headers });
 	}
 
 	//POST
 	addNoticeToInst(id: number, note: Notice){
 		this.url = this.hostname + `/institutes/${id}/notices`;
-		const headers = new HttpHeaders({
-				'Accept' : "application/json",
-				'Authorization' : "Bearer " + this.token
-		});
-		return this.http.post<ServerResponse>(this.url,note, { headers: headers});
+		return this.http.post<ServerResponse>(this.url,note, { headers: this.headers});
 	}
 	syncFileToInstitutes(fd: FormData){
 		this.url = this.hostname + `/institutes/copyFile`;
-		const headers = new HttpHeaders({
-			'Accept' : "application/json",
-			'Authorization' : "Bearer " + this.token
-		});
-		return this.http.post<ServerResponse>(this.url,fd, { headers: headers});
-	}
-	modifyInstitute(institute: Institute){
-		this.url = this.hostname + `/institutes/${institute.id}`;
-		const headers = new HttpHeaders({
-			'Accept' : "application/json",
-			'Authorization' : "Bearer " + this.token
-		});
-		return this.http.post<ServerResponse>(this.url,institute, { headers: headers});
+		return this.http.post<ServerResponse>(this.url,fd, { headers: this.headers});
 	}
 	setOssCareToInst(id: number, ossCare: OssCare){
-		//This call creates or modifies the OssCare of an institute
 		this.url = this.hostname + `/customers/institutes/${id}/osscare`;
-		const headers = new HttpHeaders({
-				'Accept' : "application/json",
-				'Authorization' : "Bearer " + this.token
-		});
-		return this.http.post<ServerResponse>(this.url,ossCare, { headers: headers});
+		return this.http.post<ServerResponse>(this.url,ossCare, { headers: this.headers});
 	}
 
 	//PUT
 	putInstallSetToSync(instituteId	: number, categoryId: number){
 		const url = `${this.hostname}/institutes/${instituteId}/categories/${categoryId}`;
-		const headers = new HttpHeaders({
-			'Content-Type': "application/json",
-			'Accept' : "application/json",
-			'Authorization' : "Bearer " + this.token
-		});
-		return this.http.put<ServerResponse>(url,null ,{ headers: headers });
+		return this.http.put<ServerResponse>(url,null ,{ headers: this.headers });
 	}
 
-	putObjectToSync(instituteId	: number, objectType: string, objectId: number|string){
+	putObjectToInstitute(instituteId	: number, objectType: string, objectId: number|string){
 		const url = `${this.hostname}/institutes/${instituteId}/objects/${objectType.toLocaleLowerCase()}/${objectId}`;
-		const headers = new HttpHeaders({
-			'Content-Type': "application/json",
-			'Accept' : "application/json",
-			'Authorization' : "Bearer " + this.token
-		});
 		console.log(url);
-		return this.http.put<ServerResponse>(url,null ,{ headers: headers });
+		return this.http.put<ServerResponse>(url,null ,{ headers: this.headers });
 	}
+	deleteObjectFromInstitute(instituteId: number, ojectType: string, objectId: number ){
+		this.url = this.hostname + `/institutes/${instituteId}/objects/${ojectType.toLocaleLowerCase()}/${objectId}`;
+		return this.http.delete<ServerResponse>(this.url, { headers: this.headers });
+	}
+
 	puCatToSync(instituteId	: number, categoryId: number|string){
 		const url = `${this.hostname}/institutes/${instituteId}/categories/${categoryId}`;
-		const headers = new HttpHeaders({
-			'Content-Type': "application/json",
-			'Accept' : "application/json",
-			'Authorization' : "Bearer " + this.token
-		});
 		console.log(url);
-		return this.http.put<ServerResponse>(url,null ,{ headers: headers });
+		return this.http.put<ServerResponse>(url,null ,{ headers: this.headers });
 	}
 	updateById(instituteId: number){
 		const url = `${this.hostname}/institutes/${instituteId}/update`;
@@ -238,24 +177,7 @@ export class CephalixService {
 
 
 	//DELETE
-	deleteInstituteById(instituteId: number){
-		this.url = this.hostname + `/institutes/${instituteId}`;
-		const headers = new HttpHeaders({
-			'Content-Type': "application/json",
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.delete<ServerResponse>(this.url, { headers: headers });
-	}
-	deleteObjectInstitute(instituteId: number, objectId: number, ojectType: string){
-		this.url = this.hostname + `/institutes/${instituteId}/objects/${ojectType.toLocaleLowerCase()}/${objectId}`;
-		const headers = new HttpHeaders({
-			'Content-Type': "application/json",
-			'Accept': "application/json",
-			'Authorization': "Bearer " + this.token
-		});
-		return this.http.delete<ServerResponse>(this.url, { headers: headers });
-	}
+
 	deleteNote(id : number){
 		this.url = this.hostname + `/institutes/notices/${id}`;
 		const headers = new HttpHeaders({
