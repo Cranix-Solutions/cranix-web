74c00449985d04ae2dfb1920461e3409aa16892f
Helmuth Varkoly <helmuth@varkoly.de>
2020-05-24 19:33:44 +0200

    looks do matter...

--- src/app/pipes/ag-action-renderer.ts
+++ src/app/pipes/ag-action-renderer.ts
@@ -5,23 +5,29 @@ import { ICellRendererAngularComp } from "ag-grid-angular";
 @Component({
     selector: 'action-cell',
     template: `
-        <ion-button style="padding-horizontal : 2px" fill="clear" size="small" (click)="details()" matTooltip="{{'edit' | translate }}">
+    <div class="name-action">
+        <!--div>
+        <ion-note >{{ name }}</ion-note>
+        </div-->
+        <ion-button fill="clear" size="small" (click)="details()" matTooltip="{{'edit' | translate }}">
              <ion-icon name="build-sharp"></ion-icon>
         </ion-button>
         <ion-button fill="clear" size="small" (click)="openAction($event)" matTooltip="{{'Apply actions on the selected objects' | translate }}">
             <ion-icon  name="ellipsis-vertical-sharp"></ion-icon> 
         </ion-button>
-        <!--ion-button style="padding-horizontal : 2px" fill="clear"  size="small" (click)="delete()" matTooltip="{{'delete' | translate }}">
+        <!--ion-button fill="clear"  size="small" (click)="delete()" matTooltip="{{'delete' | translate }}">
             <ion-icon color="danger" name="trash-outline" ></ion-icon>
         </ion-button-->
+        </div>
         ` 
 })
 
 export class ActionBTNRenderer implements ICellRendererAngularComp {
     private params: any;
-
+    name; 
     agInit(params: any ): void {
         this.params = params;
+        this.name= params.data.name;
     }
 
     public details() {
--- src/app/pipes/ag-uuid-renderer.ts
+++ src/app/pipes/ag-uuid-renderer.ts
@@ -14,11 +14,12 @@ export class WindowRef {
 }
 @Component({
     selector: 'uuid-cell',
-    template: `
+    template: `<div class="uuid-flex">
     <ion-button color="secondary" fill="clear" size="small"  (click)="routeSchool()" matTooltip="{{'Connect the institute in a separate window.' | translate }}">
     <ion-icon name="create-outline" style="height:15px;width:15px"></ion-icon>
     </ion-button>
     {{ params.data.uuid }}
+    </div>
     `
 })
 export class InstituteUUIDCellRenderer implements ICellRendererAngularComp, OnDestroy {
