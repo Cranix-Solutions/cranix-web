eccb86dc53ac3b14fc8d6ae74aa1644ded3a0f8f
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-06-23 15:28:28 +0200

    Use acl for allow/denie software download.

--- src/app/protected/cranix/softwares/softwares.page.ts
+++ src/app/protected/cranix/softwares/softwares.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthenticationService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'cranix-softwares',
@@ -8,6 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class SoftwaresPage {
   constructor(
-    public translateService: TranslateService
+    public translateService: TranslateService,
+    public authService: AuthenticationService
   ) {}
 }
