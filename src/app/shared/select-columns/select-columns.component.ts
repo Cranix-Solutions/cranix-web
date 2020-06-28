f87396a029185f898fb46c40fc52dbdd9c4f118b
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-06-28 21:50:09 +0200

    Code clean up

--- src/app/shared/select-columns/select-columns.component.ts
+++ src/app/shared/select-columns/select-columns.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NavParams,  ModalController } from '@ionic/angular';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { NavParams, ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -15,23 +15,23 @@ export class SelectColumnsComponent implements OnInit {
   editForm: FormGroup;
   objectPath: string = "";
   columns: string[] = [];
-  selected : string[] = [];
+  selected: string[] = [];
 
-  constructor(  
+  constructor(
     public formBuilder: FormBuilder,
     private navParams: NavParams,
     private modalController: ModalController,
     private router: Router,
     private storage: Storage,
-    public  translateService: TranslateService) {
-      this.columns  =  this.navParams.get('columns');
-      this.selected  =  this.navParams.get('selected');
-      this.objectPath  =  this.navParams.get('objectPath');
-     }
+    public translateService: TranslateService) {
+    this.columns = this.navParams.get('columns');
+    this.selected = this.navParams.get('selected');
+    this.objectPath = this.navParams.get('objectPath');
+  }
 
   ngOnInit() {
     var object: any = {};
-    for( let key of  this.columns ) {
+    for (let key of this.columns) {
       object[key] = this.selected.indexOf(key) != -1;
     }
     console.log("Object:" + object);
@@ -44,13 +44,13 @@ export class SelectColumnsComponent implements OnInit {
   onSubmit(object) {
     console.log(object);
     var myArray: string[] = [];
-    for( let key of this.columns) {
-      if(object[key]){
+    for (let key of this.columns) {
+      if (object[key]) {
         myArray.push(key);
       }
     }
     console.log(myArray);
-    this.storage.set(this.objectPath,JSON.stringify(myArray));
+    this.storage.set(this.objectPath, JSON.stringify(myArray));
     this.modalController.dismiss(myArray);
-  } 
+  }
 }
