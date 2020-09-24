049e1b5d19ad60ece176999173fedaca9994e9f7
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-09-24 17:43:57 +0200

    Enhance translation.

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -23,7 +23,6 @@ export class UtilsService {
                         url = protocol + "//" + hostname + "/api";
 		}
                 //console.log("From utils: " + url);
-                return "https://78.94.80.30:14444/api";
                 return url ;
         }
         public log(args) {
