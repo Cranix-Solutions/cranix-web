c53445080595b6f1728602f9876ca0043cec7871
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-11-06 11:48:01 +0100

    Use ion-content instead of ioc-card-content for global configuration. Sort the entries.

--- src/app/protected/cranix/system/config/system-config.component.ts
+++ src/app/protected/cranix/system/config/system-config.component.ts
@@ -22,7 +22,9 @@ export class SystemConfigComponent implements OnInit {
 
   ngOnInit() {
     let sub = this.systemService.getSystemConfiguration().subscribe(
-      (val) => { this.configs = val },
+    (val) => {
+      val.sort((a, b) => (a.key > b.key) ? 1 : (b.key > a.key) ? -1 : 0)
+      this.configs = val },
       (err) => { console.log(err) },
       () => { sub.unsubscribe() }
     )
