807999e3fca1a051b88f30d70abd749eafd79f68
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2021-02-27 22:21:17 +0100

    Code clean up

--- src/app/services/hwconfs.service.ts
+++ src/app/services/hwconfs.service.ts
@@ -11,7 +11,7 @@ import { AuthenticationService } from './auth.service';
 
 export interface CloneCommand {
 	deviceIds: number[],
-	partitionIds: number [],
+	partitionIds: number[],
 	multiCast: boolean,
 
 }
@@ -19,71 +19,71 @@ export interface CloneCommand {
 @Injectable()
 export class HwconfsService {
 
-        hostname: string;
-        url: string;
+	hostname: string;
+	url: string;
 
-        constructor(
-                private utilsS: UtilsService,
-                private http: HttpClient,
-                private authService: AuthenticationService) {
-                        this.hostname = this.utilsS.hostName();
-        }
+	constructor(
+		private utilsS: UtilsService,
+		private http: HttpClient,
+		private authService: AuthenticationService) {
+		this.hostname = this.utilsS.hostName();
+	}
 
-	startCloning(clone: CloneCommand,hwconfId: number ){
+	startCloning(clone: CloneCommand, hwconfId: number) {
 		this.url = `${this.hostname}/clonetool/${hwconfId}/cloning`;
 		return this.http.post<ServerResponse>(this.url, clone, { headers: this.authService.headers });
 	};
 
 	//GET Calls
 
-	getMembers(id: number){
+	getMembers(id: number) {
 		this.url = `${this.hostname}/devices/byHWConf/${id}`;
 		console.log(this.url);
 		return this.http.get<Device[]>(this.url, { headers: this.authService.headers });
 	}
-	getHwconfById(id: number){
+	getHwconfById(id: number) {
 		this.url = `${this.hostname}/clonetool/${id}`;
 		console.log(this.url);
 		return this.http.get<Hwconf>(this.url, { headers: this.authService.headers });
 	}
 
-	getMultiDevs(){
+	getMultiDevs() {
 		this.url = `${this.hostname}/clonetool/multicastDevices`;
 		console.log(this.url);
 		return this.http.get<string>(this.url, { headers: this.authService.headers });
 	}
-	isMaster(devId: number){
+	isMaster(devId: number) {
 		this.url = `${this.hostname}/clonetool/devices/${devId}/isMaster`;
 		console.log(this.url);
 		return this.http.get(this.url, { headers: this.authService.headers });
 	}
 
-	getHWConfs(){
+	getHWConfs() {
 		this.url = `${this.hostname}/clonetool/all`;
 		console.log(this.url);
 		return this.http.get<Hwconf[]>(this.url, { headers: this.authService.headers });
 	}
 	//PUT calls
 
-	setMaster(devId: number, value: number){
+	setMaster(devId: number, value: number) {
 		this.url = `${this.hostname}/clonetool/devices/${devId}/setMaster/${value}`;
 		console.log(this.url);
-		return this.http.put<ServerResponse>(this.url, null, {headers: this.authService.headers});
+		return this.http.put<ServerResponse>(this.url, null, { headers: this.authService.headers });
 	}
-	writeHWToMulti(hwId: number){
+	writeHWToMulti(hwId: number) {
 		this.url = `${this.hostname}/clonetool/${hwId}/cloning/1`;
 		console.log(this.url);
-		return this.http.put<ServerResponse>(this.url, null, {headers: this.authService.headers});
+		return this.http.put<ServerResponse>(this.url, null, { headers: this.authService.headers });
 	}
 
-	startMultiCast(parId: number, net: string){
+	startMultiCast(parId: number, net: string) {
 		this.url = `${this.hostname}/clonetool/partitions/${parId}/multicast/${net}`;
 		console.log(this.url);
-		return this.http.put<ServerResponse>(this.url, null, {headers: this.authService.headers});
+		return this.http.put<ServerResponse>(this.url, null, { headers: this.authService.headers });
 	}
 	//DELETE calls
 
-	deleteHwconfById(hwconfId: number){
+	deleteHwconfById(hwconfId: number) {
 		this.url = this.hostname + `/clonetool/${hwconfId}`;
 		return this.http.delete<ServerResponse>(this.url, { headers: this.authService.headers });
 	}
