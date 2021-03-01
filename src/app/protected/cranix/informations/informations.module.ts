f984cea0bc30ee1fa7ff3e8bdd28f2d9e7f5240d
Dipl.-Ing. Peter Varkoly <pvarkoly@cephalix.eu>
2021-03-01 14:12:07 +0100

    Integration of ngx-quill

--- src/app/protected/cranix/informations/informations.module.ts
+++ src/app/protected/cranix/informations/informations.module.ts
@@ -7,6 +7,7 @@ import { CranixSharedModule } from 'src/app/shared/cranix-shared.module';
 import { TranslateService } from '@ngx-translate/core';
 import { PipesModule } from 'src/app/pipes/pipe-modules';
 import { InformationsComponent, AddInfoPage } from 'src/app/protected/cranix/informations/informations.component'
+import { QuillModule } from 'ngx-quill';
  
 const routes: Routes = [
   {
@@ -19,9 +20,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
+    CranixSharedModule,
     IonicModule,
-    CranixSharedModule
+    QuillModule.forRoot(),
+    RouterModule.forChild(routes)
   ],
   declarations: [InformationsComponent, AddInfoPage ],
   providers: [TranslateService]
