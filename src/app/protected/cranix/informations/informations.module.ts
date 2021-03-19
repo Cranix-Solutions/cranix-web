52e5f4a27909b5e1f6ffc7873cfcc0b52a47ce9e
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-03-19 20:57:48 +0100

    Try to make link an formula for quill-editor

--- src/app/protected/cranix/informations/informations.module.ts
+++ src/app/protected/cranix/informations/informations.module.ts
@@ -7,6 +7,7 @@ import { CranixSharedModule } from 'src/app/shared/cranix-shared.module';
 import { TranslateService } from '@ngx-translate/core';
 import { InformationsComponent, AddEditInfoPage, ShowResponses } from 'src/app/protected/cranix/informations/informations.component'
 import { QuillModule } from 'ngx-quill';
+import { mathToolbarOptions } from 'src/app/shared/models/constants';
  
 const routes: Routes = [
   {
@@ -21,7 +22,9 @@ const routes: Routes = [
     CommonModule,
     CranixSharedModule,
     IonicModule,
-    QuillModule.forRoot(),
+    QuillModule.forRoot({
+      modules: { toolbar: mathToolbarOptions},
+    }),
     RouterModule.forChild(routes)
   ],
   declarations: [InformationsComponent, AddEditInfoPage,  ShowResponses],
--- src/app/shared/models/constants.ts
+++ src/app/shared/models/constants.ts
@@ -8,4 +8,19 @@ export const simpleToolbarOptions = [
     [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
     [{ 'font': [] }],
     [{ 'align': [] }]
+    ['link'],
+];
+
+export const mathToolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    ['link'],
+    ['formula']
 ];
