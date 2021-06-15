e2f924d27858bc3d48f02eabc923070ff9c1b36b
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-06-15 23:45:17 +0200

    Do not show inactiv parameters. For ACT access in room there is no login, proxy, portal, printing and direct attributes.

--- src/app/pipes/ag-yesno-renderer.ts
+++ src/app/pipes/ag-yesno-renderer.ts
@@ -5,12 +5,12 @@ import { ICellRendererAngularComp } from "ag-grid-angular";
 @Component({
     selector: 'yesno-cell',
     template: `
-        <ion-button *ngIf="params.data && active" fill="clear" size="small"
+        <ion-button *ngIf="params.value != null && active" fill="clear" size="small"
             [color]="params.value ? 'success' : 'danger'" (click)="toggle()">
             <ion-icon *ngIf="params.value" name="checkmark-circle" slot="icon-only"></ion-icon>
             <ion-icon *ngIf="!params.value" name="close-circle" slot="icon-only"></ion-icon>
         </ion-button>
-        <div *ngIf="params.data && !active">
+        <div *ngIf="params.value != null && !active">
              <ion-icon *ngIf="params.value" name="checkmark-sharp" color="success"></ion-icon>
              <ion-icon *ngIf="!params.value" name="close" color="danger"></ion-icon>
         </div>
