e6e1c30f47a874d7348057f4c3aaa5fb572b05cf
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-09-03 14:06:32 +0200

    Implement deleting of partitions.

--- src/app/protected/cranix/hwconfs/details/edit/hwconf-edit.page.ts
+++ src/app/protected/cranix/hwconfs/details/edit/hwconf-edit.page.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { GenericObjectService } from 'src/app/services/generic-object.service';
 import { Hwconf } from 'src/app/shared/models/data-model';
 import { FormBuilder } from '@angular/forms';
+import { HwconfsService } from 'src/app/services/hwconfs.service';
 
 @Component({
   selector: 'cranix-hwconf-edit',
@@ -16,13 +17,14 @@ export class HwconfEditPage implements OnInit {
   objectKeys: string[] = [];
   operatingSystems: string[] = ["WinBoot", "Win", "Win10", "Linux"];
   joinTypes: string[] = ["no", "Domain"];
-  tools: string[] = ["partimage", "partclone", "dd", "dd_rescue"];
+  tools: string[] = ["partimage", "partclone", "dd", "dd_rescue", "Zpartclone"];
   constructor(
     public translateService: TranslateService,
     public formBuilder: FormBuilder,
-    public objectService: GenericObjectService
+    public objectService: GenericObjectService,
+    public hwconfsService: HwconfsService
   ) {
-    this.object = <Hwconf>this.objectService.selectedObject;
+    this.object     = <Hwconf>this.objectService.selectedObject;
     this.objectKeys = Object.getOwnPropertyNames(new Hwconf());
     console.log("HwconfEditPage:");
     console.log(this.object);
@@ -36,12 +38,23 @@ export class HwconfEditPage implements OnInit {
         myObject[key] = this.object[key]
       }
     }
-    for(let part of this.object.partitions ){
-      myObject['part-'+part.id+"-name"] = part.name
-      myObject['part-'+part.id+"-description"] = part.description
-      myObject['part-'+part.id+"-os"] = part.os
-      myObject['part-'+part.id+"-tool"] = part.tool
-      myObject['part-'+part.id+"-joinType"] = part.joinType
+    let partitions = this.object.partitions.sort(function (a, b) {
+      var nameA = a.name.toUpperCase();
+      var nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    })
+    for (let part of partitions) {
+      myObject['part-' + part.id + "-name"] = part.name
+      myObject['part-' + part.id + "-description"] = part.description
+      myObject['part-' + part.id + "-os"] = part.os
+      myObject['part-' + part.id + "-tool"] = part.tool
+      myObject['part-' + part.id + "-joinType"] = part.joinType
     }
     this.editForm = this.formBuilder.group(myObject);
   }
@@ -52,25 +65,41 @@ export class HwconfEditPage implements OnInit {
   onSubmit(form) {
     console.log(form);
     let idToIndex = {};
-    for( let index in this.object.partitions ){
+    for (let index in this.object.partitions) {
       idToIndex[this.object.partitions[index].id] = index;
     }
-    this.object.name=form.name;
-    this.object.description=form.description;
-    this.object.deviceType=form.deviceType;
-    for(let key of Object.getOwnPropertyNames(form) ){
+    this.object.name = form.name;
+    this.object.description = form.description;
+    this.object.deviceType = form.deviceType;
+    for (let key of Object.getOwnPropertyNames(form)) {
       let lkey = key.split('-');
-      if(lkey[0] && lkey[0] == 'part') {
+      if (lkey[0] && lkey[0] == 'part') {
         this.object.partitions[idToIndex[lkey[1]]][lkey[2]] = form[key];
       }
     }
     console.log(this.object);
     this.objectService.modifyObjectDialog(this.object, "hwconf");
   }
-  cleanUp(ev: Event){
+  cleanUp(ev: Event) {
     //TODO
   }
-  delete(ev: Event){
-     this.objectService.deleteObjectDialog(this.object,'hwconf','');
+  delete(ev: Event) {
+    this.objectService.deleteObjectDialog(this.object, 'hwconf', '');
+  }
+  deletePartition(name) {
+    this.objectService.requestSent();
+    this.hwconfsService.deletePartition(this.object.id, name).subscribe(
+      val => {
+        this.objectService.responseMessage(val);
+        if (val.code == "OK") {
+          this.object.partitions = this.object.partitions.filter(part => part.name != name)
+          this.objectService.getAllObject('hwconf')
+          this.ngOnInit()
+        }
+      },
+      err => { this.objectService.errorMessage(err) },
+      () => {
+      }
+    )
   }
 }
