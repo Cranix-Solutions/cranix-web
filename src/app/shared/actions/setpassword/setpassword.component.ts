aa4c22809c6be962725b5a1a26db9ec10b6e69c7
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-10-04 14:51:46 +0200

    #461 "Passwort bei der ersten Anmeldung ändern" standardmäßig abschaltbar Introduce new variable DEFAULT_MUST_CHANGE. Default is yes even if not exists to stay compatible"

--- src/app/shared/actions/setpassword/setpassword.component.ts
+++ src/app/shared/actions/setpassword/setpassword.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { SystemService } from 'src/app/services/system.service';
 
 @Component({
   selector: 'cranix-setpassword',
@@ -17,12 +18,21 @@ export class SetpasswordComponent implements OnInit {
   }
 
   constructor(
-    public modalController: ModalController
+    public modalController: ModalController,
+    public systemService: SystemService
   ) { 
     console.log('type is:', this.type);
+    this.systemService.getSystemConfigValue("DEFAULT_MUST_CHANGE").subscribe(
+      (val) => {
+        console.log(val)
+        if( val == "no"){
+        this.setPassword.mustChange = false
+      }}
+    )
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   onSubmit() {
     this.modalController.dismiss(this.setPassword);
