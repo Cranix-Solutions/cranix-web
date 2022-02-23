06a291e1e95456d5d3d0adfbe01138b99628bb94
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-02-23 10:30:57 +0100

    Reorder icons after group name

--- src/app/pipes/ag-group-renderer.ts
+++ src/app/pipes/ag-group-renderer.ts
@@ -5,14 +5,14 @@ import { ICellRendererAngularComp } from "ag-grid-angular";
 @Component({
     selector: 'group-action-cell-renderer',
     template: `
-        <ion-button fill="clear" size="small" (click)="openAction($event)" matTooltip="{{'Apply actions on the selected objects' | translate }}">
-            <ion-icon  name="ellipsis-vertical-sharp"></ion-icon> 
-        </ion-button>
         <ion-button *ngIf="mayEdit" style="padding-horizontal : 2px" fill="clear" size="small" (click)="details($event)" matTooltip="{{'edit' | translate }}">
              <ion-icon name="build-sharp"></ion-icon>
         </ion-button>
         <ion-button *ngIf="mayEdit" style="padding-horizontal : 2px" fill="clear" size="small" (click)="members($event)" matTooltip="{{'Members of the group:' | translate }}">
              <ion-icon name="people-circle"></ion-icon>
+        </ion-button>
+        <ion-button fill="clear" size="small" (click)="openAction($event)" matTooltip="{{'Apply actions on the selected objects' | translate }}">
+            <ion-icon  name="ellipsis-vertical-sharp"></ion-icon>
         </ion-button>
         <ion-button *ngIf="mayEdit"  style="padding-horizontal : 2px" fill="clear"  size="small" (click)="delete($event)" matTooltip="{{'delete' | translate }}">
             <ion-icon color="danger" name="trash-outline" ></ion-icon>
