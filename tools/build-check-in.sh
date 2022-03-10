f32bf270a3f7bab3859baaa0572e83eb32051020
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-03-10 13:12:42 +0100

    New feature to show the build time of the web application

--- tools/build-check-in.sh
+++ tools/build-check-in.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 REPO="/home/OSC/home:pvarkoly:CRANIX/cranix-web"
 HERE=$( pwd )
+DATE==$( date -u +"Build at: %Y-%m-%d %H:%M" )
 git status
 
 echo -n "Can we build y/n "; read b
@@ -9,7 +10,9 @@ if [ $b != "y" ]; then
 fi
 cp src/app/services/utils.service.ts-notest src/app/services/utils.service.ts
 cp src/index.html-prod src/index.html
+sed -i "s/VERSION-PLACE-HOLDER/${DATE}/" src/app/protected/cranix/system/status/system-status.component.html
 ionic build --prod
+git stash
 echo -n "Can we checkin y/n "; read b
 if [ "${b}" != "y" ]; then
 	exit
@@ -21,4 +24,3 @@ cd ${REPO}
 osc vc
 osc ci
 cd ${HERE}
-git stash
