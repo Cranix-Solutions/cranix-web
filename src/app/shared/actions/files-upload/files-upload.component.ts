c31bd0104cde6e67a06a9aacc1026012d8089f92
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-11-15 21:03:57 +0100

    Avid error of not existing indicies. Take care the subscription will be closed.

--- src/app/shared/actions/files-upload/files-upload.component.ts
+++ src/app/shared/actions/files-upload/files-upload.component.ts
@@ -5,6 +5,7 @@ import { CrxActionMap } from '../../models/server-models';
 import { EductaionService } from 'src/app/services/education.service';
 import { GenericObjectService } from 'src/app/services/generic-object.service';
 import { LanguageService } from 'src/app/services/language.service';
+import { takeWhile } from 'rxjs/internal/operators/takeWhile';
 
 @Component({
   selector: 'cranix-files-upload',
@@ -15,8 +16,10 @@ export class FilesUploadComponent implements OnInit {
   public files: any[];
   public target1: string;
   public target: string = "";
+
   studentsOnly: boolean = true;
   cleanUp: boolean = false;
+  alive: boolean = true;
 
   @Input() actionMap: CrxActionMap;
   @Input() objectType: string;
@@ -28,6 +31,10 @@ export class FilesUploadComponent implements OnInit {
   ) {
   }
 
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
   ngOnInit() {
     this.objectType = this.objectType.replace("education/", "");
     switch (this.objectType) {
@@ -36,7 +43,7 @@ export class FilesUploadComponent implements OnInit {
         break
       }
       case 'group': {
-        if( this.actionMap.objectIds.length == 1) {
+        if (this.actionMap.objectIds.length == 1) {
           this.target1 = this.languageService.trans('to the group:')
         } else {
           this.target1 = this.languageService.trans('to the groups:')
@@ -53,11 +60,18 @@ export class FilesUploadComponent implements OnInit {
     let i = 0;
     this.educationController.uploadState.next(false)
     this.objectService.requestSent();
-    let subs = this.educationController.uploadState.subscribe(state => {
+    this.educationController.uploadState.pipe(takeWhile(() => this.alive)).subscribe(state => {
       console.log(this.actionMap)
       if (!state) {
+        console.log(i)
+        if (i == this.files.length) {
+          this.modalController.dismiss();
+          return;
+        }
+        this.educationController.uploadState.next(true);
         let fd = new FormData();
         fd.append('file', this.files[i], this.files[i].name);
+        i++;
         fd.append('objectIds', this.actionMap.objectIds.join(","));
         if (this.objectType == "group") {
           fd.append('studentsOnly', this.studentsOnly ? "true" : "false");
@@ -68,12 +82,6 @@ export class FilesUploadComponent implements OnInit {
           fd.append('cleanUp', "false");
         }
         this.educationController.uploadDataToObjects(fd, this.objectType);
-        i++;
-        if (i == this.files.length) {
-          this.modalController.dismiss();
-          subs.unsubscribe();
-          return;
-        }
       }
     });
   }
