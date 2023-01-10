e888408f93b16fbf3f8f8d9c459cb70420874258
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2023-01-10 20:13:27 +0100

    Adapt copyrights.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -312,7 +312,7 @@ export class AuthenticationService {
     async showInfo() {
         const toast = this.toastController.create({
             position: "middle",
-            message: 'Copyright 2022 Helmuth and Peter Varkoly, Nuremberg Germany<br>' +
+            message: 'Copyright 2023 Helmuth and Peter Varkoly, Nuremberg Germany<br>' +
                 'VERSION-PLACE-HOLDER',
             color: "success",
             duration: 5000
