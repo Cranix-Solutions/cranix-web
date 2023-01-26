d55a794ac20cbd9ead279faa221ca641414e9537
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2023-01-26 19:28:30 +0100

    Add missed file

---
+++ src/app/protected/edu/edu.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }            from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { LessonsModule } from './lessons/lessons.module';
+//Own Stuff
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LessonsModule
+  ]
+})
+export class EduModule { }
+
