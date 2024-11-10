b7cb17ad71259e2a18c88ca62b1ec3cb338124ab
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2024-11-10 22:39:02 +0100

    Implementing swipe gesture.

--- src/app/protected/cranix/calendar/calendar.component.ts
+++ src/app/protected/cranix/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { GestureController } from '@ionic/angular';
 import { CalendarOptions, DateSelectArg, EventChangeArg, EventClickArg, EventSourceInput } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -7,6 +8,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { CrxCalendarService } from 'src/app/services/crx-calendar.service';
+import { AuthenticationService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'crx-calendar',
@@ -40,13 +42,41 @@ export class CalendarComponent implements OnInit {
   };
 
   constructor(
-    private calendarS: CrxCalendarService
+    private authS: AuthenticationService,
+    private calendarS: CrxCalendarService,
+    private gestureCtrl: GestureController,
+    private el: ElementRef
   ) {
     this.loadDatas()
   }
 
   ngOnInit(): void {
     console.log("CalendarComponent ngOnInit called")
+    if( this.authS.isMD() ){
+      this.initializeSwipeGesture();
+    }
+  }
+
+  //Handle Swipe
+  initializeSwipeGesture() {
+    const gesture = this.gestureCtrl.create({
+      el: this.el.nativeElement,
+      gestureName: 'swipe',
+      onMove: (ev) => this.handleSwipe(ev),
+    });
+    gesture.enable();
+  }
+
+  handleSwipe(event) {
+    const threshold = 100; // Mindestdistanz für einen Swipe
+    console.log('handleSwipe called')
+    if (event.deltaX > threshold) {
+      console.log('go back')
+      this.calendarComponent.getApi().prev();
+    } else if (event.deltaX < -threshold) {
+      console.log('go ahead')
+      this.calendarComponent.getApi().next();
+    }
   }
 
   loadDatas(): void {
